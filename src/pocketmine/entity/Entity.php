5f4c3f37c97068fa60d351c208576c82ef08db65
K099 <akephisitch@gmail.com>
2016-07-18 05:26:50 +0700

    Revert "Disable"
    This reverts commit dafa7e26ed6b0a0cf7931fe233980d1e9bcbe12c.

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -1077,8 +1077,21 @@ abstract class Entity extends Location implements Metadatable{
 		if($this instanceof Player and $this->isSpectator()){
 			return;
 		}
+		if($this->getLevel()->getServer()->destroyBlockParticle and $fallDistance > 3){
+			$this->getLevel()->addParticle(new DestroyBlockParticle($this, $this->getLevel()->getBlock($this->floor()->subtract(0, 1, 0))));
+		}
 		if($this->isInsideOfWater()){
 			return;
+		}
+		$damage = floor($fallDistance - 3 - ($this->hasEffect(Effect::JUMP) ? $this->getEffect(Effect::JUMP)->getAmplifier() + 1 : 0));
+		
+		//Get the block directly beneath the player's feet, check if it is a slime block
+		if($this->getLevel()->getBlock($this->floor()->subtract(0, 1, 0)) instanceof SlimeBlock){
+			$damage = 0;
+		}
+		if($damage > 0){
+			$ev = new EntityDamageEvent($this, EntityDamageEvent::CAUSE_FALL, $damage);
+			$this->attack($ev->getFinalDamage(), $ev);
 		}
 	}
 
